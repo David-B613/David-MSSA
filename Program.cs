000379d3d5cd02c9b46cc7e8ce897af3168616b9
David Babers <david.babers@outlook.com>
2020-01-29 16:16:08 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(0)); //0
+            Console.WriteLine(Fib(5)); //5
+            Console.WriteLine(Fib(6)); //8
+        }
+        static int Fib(int n, int a = 0, int b = 1)
+        {
+            if (n <= 0) return a;
+
+            return Fib(n - 1, b, a + b);
+
+        }
+        // Count to 10 
+        static void DoAloop(int iteration)
+        {
+            // Base Case
+            if (iteration <= 0) return;
+
+
+            Console.WriteLine();
+            
+            // Inductive/Induction/Recursive/Alterntive case 
+            DoAloop(iteration - 1);
+        }
+    }
+}
